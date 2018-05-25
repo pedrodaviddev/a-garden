51ad22000996166783f590b473a7ae176d4f06a0
Pedro David Rodríguez Sánchez <pedrodavidlp3@gmail.com>
2018-05-25 16:54:57 +0200

    Implementing Ktor with Heroku

--- src/Main.kt
+++ src/Main.kt
@@ -6,7 +6,8 @@ import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, port = 8080) {
+
+    val server = embeddedServer(Netty, System.getenv("PORT")?.toInt() ?: 8080) {
         routing {
             get("/") {
                 call.respondText("Hello World!", ContentType.Text.Plain)
