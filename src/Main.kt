3f8d74cbcc1441d5181575daad0f52cdf186a885
Pedro David Rodríguez Sánchez <pedrodavidlp3@gmail.com>
2018-05-27 18:51:17 +0200

    Added put function

--- src/Main.kt
+++ src/Main.kt
@@ -2,31 +2,31 @@ import io.ktor.application.call
 import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
 import io.ktor.gson.gson
-import io.ktor.http.decodeURLQueryComponent
+import io.ktor.request.receive
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 
 fun main(args: Array<String>) {
+    val listPlants = mutableListOf<Plant>()
 
     val server = embeddedServer(Netty, System.getenv("PORT")?.toInt() ?: 8080) {
-        install(ContentNegotiation){
+        install(ContentNegotiation) {
             gson {
                 setPrettyPrinting()
             }
         }
         routing {
             get("/plants") {
-                call.respond(listOf(
-                        Plant("Orquideas", 32.4, 78.4),
-                        Plant("Arbol", 31.4, 20.4),
-                        Plant("Rosas", 29.4, 71.4),
-                        Plant("Tulipanes", 40.4, 30.4),
-                        Plant("Margaritas", 32.4, 10.4)
-                ))
+                call.respond(listPlants)
+            }
+            post("/plants") {
+                val received = call.receive<Plant>()
+                listPlants.add(received)
             }
             get("/demo") {
                 call.respondText("HELLO WORLD!")
