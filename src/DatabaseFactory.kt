571580e53b7c9d05d923d5ccaa3e3c9a64e473fd
Pedro David Rodríguez Sánchez <pedrodavidlp3@gmail.com>
2018-07-11 10:05:57 +0200

    Migrating to compatible database with heroku

--- src/DatabaseFactory.kt
+++ src/DatabaseFactory.kt
@@ -2,10 +2,8 @@ import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import kotlinx.coroutines.experimental.newFixedThreadPoolContext
 import kotlinx.coroutines.experimental.withContext
-import org.h2.tools.DeleteDbFiles
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils.create
-import org.jetbrains.exposed.sql.SchemaUtils.drop
 import org.jetbrains.exposed.sql.transactions.transaction
 import kotlin.coroutines.experimental.CoroutineContext
 
@@ -21,8 +19,10 @@ object DatabaseFactory {
 
     private fun hikari(): HikariDataSource {
         val config = HikariConfig()
-        config.driverClassName = "org.h2.Driver"
-        config.jdbcUrl = "jdbc:h2:file:~/db/data"
+        config.driverClassName = "org.postgresql.Driver"
+        config.jdbcUrl = "jdbc:postgresql://ec2-54-217-205-90.eu-west-1.compute.amazonaws.com:5432/d41c8gkcpl44pe"
+        config.username = "ltznucrbbzypnf"
+        config.password = "3d371533c7286962146147d61bf2b715f03ed85de0c61dbd922acef45ad599cc"
         config.maximumPoolSize = 3
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
